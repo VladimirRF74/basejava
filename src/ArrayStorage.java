e11d09d17a1b9d2c8521e62f684ba012fb8face1
Vladimir Konovalov <vladimir74rus@yandex.ru>
2022-02-03 15:11:04 +0500

    hw1 decision

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,56 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
+        }
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = resume;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int i;
+        for (i = 0; i < size(); i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
+            }
+        }
+        return new Resume();
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (uuid.equals(storage[i].toString())) {
+                System.arraycopy(storage, i + 1, storage, i, size());
+                storage[size()] = null;
+
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] allResume = new Resume[size()];
+        if (size() >= 0) System.arraycopy(storage, 0, allResume, 0, size());
+        return allResume;
     }
 
     int size() {
-        return 0;
+        int counter = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                counter++;
+            } else break;
+        }
+        return counter;
     }
 }
