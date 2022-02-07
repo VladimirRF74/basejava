2dc45fbf66246d8f3394b6e2963302d42066aa6e
Vladimir Konovalov <vladimir74rus@yandex.ru>
2022-02-07 05:22:48 +0500

    lession02 изменил метод save

--- src/com/urise/webapp/MainTestArrayStorage.java
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -19,21 +19,25 @@ public class MainTestArrayStorage {
         ARRAY_STORAGE.save(r3);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
-        System.out.println("Size: " + ARRAY_STORAGE.size());
+        System.out.println("Size: " + ARRAY_STORAGE.getSize());
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
         ARRAY_STORAGE.delete(r1.getUuid());
+        printAll();
+        ARRAY_STORAGE.save(r2);
         printAll();
         ARRAY_STORAGE.update(r3);
         printAll();
         ARRAY_STORAGE.update(r1);
         printAll();
         ARRAY_STORAGE.clear();
-        printAll();
+        ARRAY_STORAGE.delete(r1.getUuid());
         ARRAY_STORAGE.update(r1);
+        printAll();
 
-        System.out.println("Size: " + ARRAY_STORAGE.size());
+
+        System.out.println("Size: " + ARRAY_STORAGE.getSize());
     }
 
     static void printAll() {
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -5,7 +5,7 @@ import com.urise.webapp.model.Resume;
 import java.util.Arrays;
 
 public class ArrayStorage {
-    private final Resume[] storage = new Resume[10000];
+    private final Resume[] storage = new Resume[3];
     private int size = 0;
 
     public void clear() {
@@ -14,11 +14,26 @@ public class ArrayStorage {
     }
 
     public void save(Resume resume) {
-        if (size == 0 || storage.length > size) {
+        boolean rb = false;
+        if (size == 0) {
             storage[size] = resume;
             size++;
-        } else System.out.println("Sorry, no place!");
+            return;
+        } else if (size >= storage.length) {
+            System.out.println("Sorry, array is full");
+            return;
+        } else for (int i = 0; i < size; i++) {
+            rb = storage[i].getUuid().equals(resume.getUuid());
+        }
+        if (rb) {
+            System.out.printf("Sorry, %s already available \n", resume);
+        } else {
+            storage[size] = resume;
+            size++;
+        }
+
     }
+
 
     public void update(Resume resume) {
         int counter = 0;
@@ -27,11 +42,11 @@ public class ArrayStorage {
         } else for (int i = 0; i < size; i++) {
             if (resume.getUuid().equals(storage[i].toString())) {
                 storage[i] = resume;
-                break;
+                return;
             } else if (size > i) {
                 counter++;
                 if (size == counter) {
-                    System.out.println("Sorry, what we are looking for is not here!");
+                    System.out.printf("Sorry, what we are looking for is not %s! \n", resume.getUuid());
                 }
             }
         }
@@ -39,11 +54,12 @@ public class ArrayStorage {
 
     public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].toString().equals(uuid)) {
+            if (uuid.equals(storage[i].toString())) {
                 return storage[i];
             }
         }
-        return new Resume("Sorry, he's not here!");
+        System.out.printf("Sorry, he's not %s! \n", uuid);
+        return null;
     }
 
     public void delete(String uuid) {
@@ -52,24 +68,19 @@ public class ArrayStorage {
                 storage[i] = storage[size - 1];
                 storage[size - 1] = null;
                 size--;
-                break;
+                return;
             }
         }
+        System.out.printf("Sorry, %s failed to delete \n", uuid);
     }
 
     public Resume[] getAll() {
-        Resume[] allResume = new Resume[size()];
+        Resume[] allResume = new Resume[size];
         if (size > 0) System.arraycopy(storage, 0, allResume, 0, size);
         return allResume;
     }
 
-    public int size() {
-        size = 0;
-        for (Resume resume : storage) {
-            if (resume != null) {
-                size++;
-            } else break;
-        }
+    public int getSize() {
         return size;
     }
 }
